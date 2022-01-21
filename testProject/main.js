cc828cbe269c00f2c2183efbf41eb16b1828d2b6
Fredrik93 <fredrikullman93@gmail.com>
2022-01-21 07:41:40 +0100

    initial

---
+++ testProject/main.js
@@ -0,0 +1,33 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: './assets/socks-green.jpg',
+        linkurl: 'https://stackoverflow.com/questions/57927644/nodejs-express-how-to-get-an-element-which-made-an-http-get-request',
+        inventory: 100,
+        inStock: true,
+        onSale: false,
+        details: ["80% Cotton", "20% Polyester", "Gender-Neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            },
+        ]
+    }
+})
+
+var todoList = new Vue({
+    el: '#todo-list',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    }
+})
